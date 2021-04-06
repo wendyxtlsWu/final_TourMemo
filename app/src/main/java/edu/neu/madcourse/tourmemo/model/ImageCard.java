ff1f5903ea6be82d98c617027ee4ce23da0f3ee7
wujingjing <wendyxtls@gmail.com>
2021-04-05 22:44:53 -0700

    create ImageCard.java-jjw

---
+++ app/src/main/java/edu/neu/madcourse/tourmemo/model/ImageCard.java
@@ -0,0 +1,21 @@
+package edu.neu.madcourse.tourmemo.model;
+
+public class ImageCard {
+
+    private int img;
+
+    public ImageCard(int img) {
+        this.img = img;
+    }
+
+    public ImageCard() {
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
