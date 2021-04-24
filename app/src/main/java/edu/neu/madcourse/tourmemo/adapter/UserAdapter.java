e49407386bed67c7afea525b2cc6eb3df6e7fd5f
zihxiao <zihnxiao@gmail.com>
2021-04-24 02:09:12 -0700

    Fix notification bugs

--- app/src/main/java/edu/neu/madcourse/tourmemo/adapter/UserAdapter.java
+++ app/src/main/java/edu/neu/madcourse/tourmemo/adapter/UserAdapter.java
@@ -150,11 +150,11 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
     private void addNotification(String userId) {
         HashMap<String, Object> map = new HashMap<>();
 
-        map.put("userid", userId);
+        map.put("userid", FirebaseAuth.getInstance().getCurrentUser().getUid());
         map.put("text", "started following you.");
         map.put("postid", "");
         map.put("isPost", false);
 
-        FirebaseDatabase.getInstance().getReference().child("Notifications").child(firebaseUser.getUid()).push().setValue(map);
+        FirebaseDatabase.getInstance().getReference().child("Notifications").child(userId).push().setValue(map);
     }
 }
